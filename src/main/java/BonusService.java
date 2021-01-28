8fd1114ff99a802877c8c1be0a189bcc180ffc82
Olga Tarasova <bos.olga18@gmail.com>
2021-01-28 16:47:57 +0300

    New file

---
+++ src/main/java/BonusService.java
@@ -0,0 +1,16 @@
+import javax.annotation.CheckReturnValue;
+
+public class BonusService {
+  @CheckReturnValue
+  public long calculate(long amount, boolean registered) {
+    int percent = registered ? 3 : 1;
+    long bonus = amount * percent / 100 / 100;
+    long limit = 500;
+    if (bonus > limit) {
+      bonus = limit;
+    }
+    return bonus;
+  }
+}
+
+
